831edeb9506045d6e9d9f7b13ded48456d66e88d
David Puleri <david@puleri.com>
2014-09-14 10:35:39 +0100

    Initial set up with autoloader

---
+++ src/Builder/Products.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Ebay\Mfs\Builder;
+
+class Products {
+
+} 
---
+++ tests/Feeds/ProductsTest.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: david
+ * Date: 14/09/14
+ * Time: 10:09
+ */
+
+class ProductsTest extends \PHPUnit_Framework_TestCase {
+
+    function testAssertTrueTest(){
+
+
+        $this->assertEquals('1', '1');
+
+    }
+
+} 
